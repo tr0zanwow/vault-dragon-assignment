daca112afd0d4a92540568e6e9995016f9dd6061
tr0zanwow <pranil.rathod93@gmail.com>
2020-02-29 10:16:28 +0530

    initial commit

---
+++ handler.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+AWS.config.update({region: "ap-south-1"});
+
+module.exports.getKey = (event, context,  callback) => {
+if(event.mykey === "")
+{
+callback("Error : No Input specified");
+}
+else{
+var documentClient = new AWS.DynamoDB.DocumentClient({region : "ap-south-1"});
+
+const temp1 = {
+  TableName: "vault-dragon-data",
+  Key: {
+    key: event.mykey
+  }
+};
+
+  documentClient.get(temp1,(err,data) =>{
+    if(err){
+      console.log(err)
+    }
+    callback(null,data.Item);
+  })
+}
+};
