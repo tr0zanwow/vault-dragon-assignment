835f715dde5f66196cbda8d63e6e790c34d9ac10
tr0zanwow <pranil.rathod93@gmail.com>
2020-02-29 15:46:02 +0530

    initial commit

--- handler.js
+++ handler.js
@@ -1,53 +1,50 @@
 'use strict';
-
+//Imports
 const AWS = require('aws-sdk');
 
-AWS.config.update({region: "ap-south-1"});
+//AWS DynamoDB configurations
+AWS.config.update({
+    region: "ap-south-1"
+});
+var documentClient = new AWS.DynamoDB.DocumentClient({
+    region: "ap-south-1"
+});
 
-module.exports.getKey = (event, context,  callback) => {
-if(event.mykey === "")
-{
-callback("Error : No Input specified");
-}
-else{
-var documentClient = new AWS.DynamoDB.DocumentClient({region : "ap-south-1"});
+//Get Data module - GET Param Handling
+module.exports.getKey = (event, context, callback) => {
+    if (event.mykey == "") {
+        callback("Error : No Input specified");
+    } else {
 
-const temp1 = {
-  TableName: "vault-dragon-data",
-  Key: {
-    key: event.mykey
-  }
+        const temp1 = {
+            TableName: "vault-dragon-data",
+            Key: {
+                key: event.mykey
+            }
+        };
+        documentClient.get(temp1, (err, data) => {
+            if (err) {
+                console.log(err)
+            }
+            callback(null, data);
+        })
+    }
 };
 
-  documentClient.get(temp1,(err,data) =>{
-    if(err){
-      console.log(err)
+//Set Data Module - POST Param Handling
+module.exports.setKey = async (event, context, callback) => {
+    const temp2 = {
+        TableName: "vault-dragon-data",
+        Item: {
+            key: event.mykey,
+            timeStamp: event.timestamp,
+            value: event.value
+        }
+    };
+    try {
+        const data = await documentClient.put(temp2).promise();
+        callback(null, data);
+    } catch (error) {
+        callback("Problem adding Data")
     }
-    callback(null,data);
-  })
-}
 };
-
-module.exports.setKey = (event, context,  callback) => {
-  if(event.mykey === "")
-  {
-  callback("Error : No Input specified");
-  }
-  else{
-  var documentClient = new AWS.DynamoDB.DocumentClient({region : "ap-south-1"});
-  
-  const temp1 = {
-    TableName: "vault-dragon-data",
-    Key: {
-      key: event.mykey
-    }
-  };
-  
-    documentClient.get(temp1,(err,data) =>{
-      if(err){
-        console.log(err)
-      }
-      callback(null,data);
-    })
-  }
-  };
